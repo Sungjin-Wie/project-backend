8a4bc317375612d8bbcf4a0bccd003b5f7e0d829
Sungjin-Wie <tmdwls1203@gmail.com>
2022-08-25 00:22:46 +0900

    move redis to other instance

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,5 +1,8 @@
 import * as redis from "redis";
-export const redisClient = redis.createClient();
+export const redisClient = redis.createClient({
+  host: "192.168.0.16",
+  port: 6379,
+});
 export const init = async () => {
   try {
     await redisClient.connect();
