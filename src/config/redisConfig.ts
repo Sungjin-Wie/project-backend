84bf4d7c5a8d4804d8e391f56d2e72ac2270abdc
sjwie <sjwie@dtol.co.kr>
2022-08-25 17:01:51 +0900

    rollback redis option

--- src/config/redisConfig.ts
+++ src/config/redisConfig.ts
@@ -1,8 +1,9 @@
 import * as redis from "redis";
-export const redisClient = redis.createClient({
-  url: `redis://${process.env.REDIS_URL}:6379`,
-  password: process.env.REDIS_PASSWORD,
-});
+// export const redisClient = redis.createClient({
+//   url: `redis://${process.env.REDIS_URL}:6379`,
+//   password: process.env.REDIS_PASSWORD,
+// });
+export const redisClient = redis.createClient();
 export const init = async () => {
   try {
     await redisClient.connect();
