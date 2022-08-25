39e35a2f8b3a203095d950aef5c4f7885ab28d6d
sjwie <sjwie@dtol.co.kr>
2022-08-25 16:56:09 +0900

    add password for redis

--- src/config/redisConfig.ts
+++ src/config/redisConfig.ts
@@ -1,6 +1,7 @@
 import * as redis from "redis";
 export const redisClient = redis.createClient({
   url: `redis://${process.env.REDIS_URL}:6379`,
+  password: process.env.REDIS_PASSWORD,
 });
 export const init = async () => {
   try {
