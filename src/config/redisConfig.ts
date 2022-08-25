2478cc3dd8e634b1d2968c71e1e01208d3f753c8
sjwie <sjwie@dtol.co.kr>
2022-08-25 17:25:29 +0900

    modified redis config

--- src/config/redisConfig.ts
+++ src/config/redisConfig.ts
@@ -1,9 +1,8 @@
 import * as redis from "redis";
-// export const redisClient = redis.createClient({
-//   url: `redis://${process.env.REDIS_URL}:6379`,
-//   password: process.env.REDIS_PASSWORD,
-// });
-export const redisClient = redis.createClient();
+export const redisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_URL}:6379`,
+  password: process.env.REDIS_PASSWORD,
+});
 export const init = async () => {
   try {
     await redisClient.connect();
