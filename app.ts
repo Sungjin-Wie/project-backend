080026feecebfc7a761c0033347ef3e112afd85d
sjwie <sjwie@dtol.co.kr>
2022-08-26 09:31:26 +0900

    replace redis to ioredis

--- app.ts
+++ app.ts
@@ -13,6 +13,7 @@ const env = process.env.NODE_ENV ?? "production";
 dotenv.config({ path: `./.env.${env}` });
 console.log(env, "mode");
 console.log("starting server at port", process.env.PORT);
+console.log(process.env.REDIS_URL, process.env.REDIS_PASSWORD);
 init();
 console.log("starting redis in", process.env.REDIS_URL);
 const app = express();
--- src/config/redisConfig.ts
+++ src/config/redisConfig.ts
@@ -1,11 +1,17 @@
 import * as redis from "redis";
-export const redisClient = redis.createClient({
-  url: process.env.REDIS_URL,
+import Redis from "ioredis";
+// export const redisClient = redis.createClient({
+//   url: process.env.REDIS_URL,
+//   password: process.env.REDIS_PASSWORD,
+// });
+export const redisClient = new Redis({
+  host: process.env.REDIS_HOST,
+  port: 6379,
   password: process.env.REDIS_PASSWORD,
 });
 export const init = async () => {
   try {
-    await redisClient.connect();
+    // await redisClient.connect();
   } catch (e) {
     throw e;
   }
