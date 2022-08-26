d1ae4ebcd6a8df3be83f94ed938f448e3401a4b5
sjwie <sjwie@dtol.co.kr>
2022-08-26 10:22:42 +0900

    done redisconfig finally

--- src/config/redisConfig.ts
+++ src/config/redisConfig.ts
@@ -1,18 +1,9 @@
-// import * as redis from "redis";
 import Redis from "ioredis";
-// export const redisClient = redis.createClient({
-//   url: process.env.REDIS_URL,
-//   password: process.env.REDIS_PASSWORD,
-// });
-export const redisClient = new Redis({
-  host: process.env.REDIS_HOST,
-  port: 6379,
-  password: process.env.REDIS_PASSWORD,
-});
-export const init = async () => {
-  try {
-    // await redisClient.connect();
-  } catch (e) {
-    throw e;
-  }
-};
+import dotenv from "dotenv";
+const env = process.env.NODE_ENV ?? "production";
+dotenv.config({ path: `./.env.${env}` });
+const host = process.env.REDIS_HOST as string;
+const port = 6379;
+console.log(host, port);
+const redisUrl = process.env.REDIS_URL as string;
+export const redisClient = new Redis(redisUrl);
